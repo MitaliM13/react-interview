5c8d4e68ac1e23a988c10397396c22b8ce93800f
MitaliM13 <mitalimaurya@kccemsr.edu.in>
2025-03-21 16:48:08 +0530

    Start sync and async

---
+++ BuiltInConstructor/index.js
@@ -0,0 +1,9 @@
+//Built-in constructors, also known as native constructors, are standard constructors provided by JavaScript that allow you to create objects of various data types. These constructors are available globally and do not need to be explicitly defined. They provide a convenient way to create instances of primitive data types and built-in objects
+
+const num1 = new Number(10)
+const num2 = 10
+const str = new String("Mitali")
+
+console.log(typeof num1);
+console.log(typeof num2);
+console.log(str);
